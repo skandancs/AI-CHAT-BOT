8a9993b7ae838efc34cd0eae1e387381456b2c27
skandancs <skandancs@gmail.com>
2026-01-20 15:10:14 +0530

    Update LLM_BOT.py

--- LLM_BOT.py
+++ LLM_BOT.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from groq import Groq
 
-st.set_page_config("PragyanAI Content Generator", layout="wide")
+st.set_page_config("Bodha AI by Skandan", layout="wide")
 st.title("Bodha AI")
 
 client = Groq(api_key=st.secrets["GROQ_API_KEY"])
